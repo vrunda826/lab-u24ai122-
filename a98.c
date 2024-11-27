d1576382958a30124512dabb65a6f1cfb4e9cbfe
vrunda826 <vrundapatel826@gmail.com>
2024-11-27 07:28:58 +0530

    Add files via upload

--- a98.c
+++ a98.c
@@ -1,25 +1,67 @@
-#include<stdio.h>
-int main()
-{
-    int matrix1[3][3],matrix2[3][3],multi[3][3];
-    printf("enter first matrix\n");
-    scanf()
-    for(int i=0; i<3; i++)
-    {
-        for( int j=0; j<3; j++)
-        {
-            scanf("%d", &matrix1[i][j]);
-        }
-    }
-    for(int i=0; i<3; i++)
-    {
-        for( int j=0; j<3; j++)
-        {
-            scanf("%d", &matrix2[i][j]);
-        }
-    }
-    int *ptr1,*ptr2;
-    ptr1=matrix1;
-    ptr2=matrix2;
+//Write a C program to find the product of 2 matrics using pointer and using dynamic memory alocation.
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int n;
+    printf("Enter dimension : ");
+    scanf("%d", &n);
+
+    int rows = n, cols = n; 
+
+    //1
+        int** arr1= (int**)malloc(rows * sizeof(int*));
+        
+        for (int i = 0; i < rows; i++) {
+            arr1[i] = (int*)malloc(cols * sizeof(int));
+        }
+
+        printf("Enter first array... \n");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                scanf("%d", &arr1[i][j]);
+            }
+        }
+
+    //2
+        int** arr2= (int**)malloc(rows * sizeof(int*));
+        
+        for (int i = 0; i < rows; i++) {
+            arr2[i] = (int*)malloc(cols * sizeof(int));
+        }
+
+        printf("Enter second array... \n");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                scanf("%d", &arr2[i][j]);
+            }
+        }
+        
+    //3
+        int** arr3= (int**)malloc(rows * sizeof(int*));
+        
+        for (int i = 0; i < rows; i++) {
+            arr3[i] = (int*)malloc(cols * sizeof(int));
+        }
+
+    for(int i=0; i<rows; i++) {
+        for(int j=0; j<cols; j++) {
+            arr3[i][j]=0;
+            for(int k=0; k<n; k++) {
+                arr3[i][j]+=arr1[i][k]*arr2[k][j];
+            }
+        }
+    }
+
+    printf("Multiplication......\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d\t", arr3[i][j]);
+        }
+        printf("\n");
+    }
+    
+    return 0;
 }
